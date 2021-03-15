85002ffde4807248b22527b83eedddcce39e7fc9
Swiatly, Krzysztof <Krzysztof.Swiatly@harman.com>
2021-03-15 15:32:20 +0100

    Added jenkins_pipeline_multibranch

---
+++ jenkins_pipeline_multibranch/script.groovy
@@ -0,0 +1,15 @@
+def buildApp() {
+    echo 'Building an app in Groovy script'
+    //code logic here
+}
+
+def deployApp() {
+    echo 'Deploying app'
+    //code logic here
+}
+
+def copyFilesToProd() {
+    echo 'copy files to Prod server'
+    sh 'pwd'
+}
+return this
